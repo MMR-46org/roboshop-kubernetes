e817ecaaac67a5a15c01c32e6234dab49e6514c8
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-05-09 18:15:46 -0500

    making changes to the helm.sh

--- helm.sh
+++ helm.sh
@@ -1,19 +1,18 @@
 aws eks update-kubeconfig --name dev-eks
-
 if [ "$1" == "install" ]; then
   helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
   helm repo add elastic https://helm.elastic.co
 
   helm repo update
-  helm upgrade -i install nginx-ingress  ingress-nginx/ingress-nginx
+
+  helm upgrade -i  nginx-ingress  ingress-nginx/ingress-nginx
   kubectl apply -f external-dns.yml
   helm install filebeat elastic/filebeat -f filebeat.yml
-
 fi
 
 
 if [ "$1" == "uninstall" ]; then
-  helm install nginx-ingress
+  helm uninstall nginx-ingress
   kubectl delete -f external-dns.yml
   helm uninstall filebeat
 fi
