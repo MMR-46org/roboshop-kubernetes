50c2b4ed840ca0492c86d58a628280c47d9ab065
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-05-11 22:56:19 -0500

    adding the upgrade -i to the prometheus and filebeat

--- helm.sh
+++ helm.sh
@@ -2,12 +2,14 @@ aws eks update-kubeconfig --name dev-eks
 if [ "$1" == "install" ]; then
   helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
   helm repo add elastic https://helm.elastic.co
+  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
   helm repo update
 
   helm upgrade -i  nginx-ingress  ingress-nginx/ingress-nginx
   kubectl apply -f external-dns.yml
-  helm install filebeat elastic/filebeat -f filebeat.yml
+  helm upgrade -i filebeat elastic/filebeat -f filebeat.yml
+  helm upgrade -i prometheus prometheus-community/kube-prometheus-stack -f prometheus.yml
 fi
 
 
@@ -15,6 +17,7 @@ if [ "$1" == "uninstall" ]; then
   helm uninstall nginx-ingress
   kubectl delete -f external-dns.yml
   helm uninstall filebeat
+  helm uninstall prometheus
 fi
 
 
